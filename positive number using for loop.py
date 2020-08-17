d1b4f05374a4169ed986477d503b87c97f9caf32
ANAMIKA GAUR <69541132+AnamikaGaur@users.noreply.github.com>
2020-08-17 21:37:06 +0530

    Positive Numbers(list1)
    Printing all the positive  numbers in a range using For loop

---
+++ positive number using for loop.py
@@ -0,0 +1,19 @@
+ 
+
+
+#USING FOR LOOP
+
+#Python Program to print all positive integers in a range
+
+#List of Numbers
+list1 = [12,-7,5,64,-14]
+
+#Iterating each number in list
+for num in list1:
+
+    #checking condition
+    if num > 0:
+        print(num, end = " ")
+
+
+
